e93d5bd13a3c02bafa0961b6fd071b5e03110316
Andrew Dent <andrew.dent@bbrown.com>
2024-10-14 12:27:51 -0400

    page #1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+
+}
