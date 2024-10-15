cc77b9d49b695c4f07273522a71f19d0b63da3e3
Andrew Dent <dentdrew@gmail.com>
2024-10-14 21:19:02 -0400

    pm changes

--- scripts.js
+++ scripts.js
@@ -6,3 +6,24 @@ function closePopup(popupId) {
     document.getElementById(popupId).style.display = "none";
 
 }
+
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 750) {
+        header.classList.add('header-colored');
+    } else {
+        header.classList.remove('header-colored');
+    }
+});
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
